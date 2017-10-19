26804e3521c75b88fb738a22876f3c91a53bd61a
nico_apache <nico10907@hotmail.com>
2017-10-19 18:24:31 +0000

    eliminado metodos getters

--- lib/alimento/alimento.rb
+++ lib/alimento/alimento.rb
@@ -7,21 +7,21 @@ class Food
       @nombre, @proteinas, @glucidos, @grasas = nombre, proteinas, glucidos, grasas
     end
     
-    def to_s
-        return @nombre
-    end
+    # def to_s
+    #     return @nombre
+    # end
     
-    def get_proteinas
-        return @proteinas
-    end
+    # def get_proteinas
+    #     return @proteinas
+    # end
     
-    def get_glucidos
-        return @glucidos
-    end
+    # def get_glucidos
+    #     return @glucidos
+    # end
     
-    def get_grasas
-        return @grasas
-    end
+    # def get_grasas
+    #     return @grasas
+    # end
     
     def get_format
         s="#{@nombre}: #{@proteinas} · #{@glucidos} · #{@grasas}"
@@ -29,9 +29,9 @@ class Food
     end
     
     def get_ve
-        s=(@proteinas * 4) + (@glucidos * 4) + (@grasas * 9)
-        # s+=" Kcal"
-        return s
+        sum=(@proteinas * 4) + (@glucidos * 4) + (@grasas * 9)
+        aux = "#{sum}" + " Kcal"
+        return aux
     end
     
 end
--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -10,31 +10,48 @@ RSpec.describe Alimento do
   end
   huevo = Food.new(@nombre, 14.1, 0.0, 19.5)
   
-  it "tiene un nombre" do
-    expect(huevo.to_s).not_to be nil
-    expect(huevo.to_s).to eq("Huevo frito")
+  it "debe existir un nombre para el alimento" do
+    expect(huevo.nombre).not_to be nil
+    # expect(huevo.to_s).to eq("Huevo frito")
   end
   
-  it "tiene unas proteinas en gramos" do
-    expect(huevo.get_proteinas).not_to be nil
+  it "debe existir la cantidad de proteinas del alimento en gramos" do
+    # expect(huevo.get_proteinas).not_to be nil
+    expect(huevo.proteinas).not_to be nil
   end
   
-  it "tiene unas glucidos en gramos" do
-    expect(huevo.get_glucidos).not_to be nil
+  it "debe existir la cantidad de glucidos del alimento en gramos" do
+    expect(huevo.glucidos).not_to be nil
   end
 
-  it "tiene unas grasas en gramos" do
-    expect(huevo.get_grasas).not_to be nil
+  it "debe existir la cantidad de grasas del alimento en gramos" do
+    expect(huevo.grasas).not_to be nil
+  end
+  
+  it "existe un metodo para obtener el nombre del alimento" do
+    puts huevo.nombre
+  end
+  
+  it "existe un metodo para obtener la cantidad de protinas de un alimento" do
+    puts huevo.proteinas
+  end
+  
+  it "existe un metodo para obtener la cantidad de glucidos de un alimento" do
+    puts huevo.glucidos
+  end
+  
+  it "existe un metodo para obtener la cantidad de grasas de un alimento" do
+    puts huevo.grasas
   end
   
   it "tiene un metodo para obtener alimento formateado" do
-    expect(huevo.get_format).not_to be nil
+    # expect(huevo.get_format).not_to be nil
     expect(huevo.get_format).to eq("Huevo frito: 14.1 · 0.0 · 19.5")
   end
   
   it "tiene un calcular valor energetico" do
-    expect(huevo.get_ve).not_to be nil
-    # puts huevo.get_ve
+    # expect(huevo.get_ve).not_to be nil
+    puts huevo.get_ve
   end
   
 end
