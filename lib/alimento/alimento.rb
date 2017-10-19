934bf2da7fb0b7c96731bdf2fd7f8a3f0fa7cc76
nico_apache <nico10907@hotmail.com>
2017-10-19 12:32:27 +0000

    metodo get_proteinas

--- lib/alimento/alimento.rb
+++ lib/alimento/alimento.rb
@@ -1,14 +1,18 @@
 require "alimento/alimento"
 
 class Food
-    attr_accessor :nombre
+    attr_accessor :nombre, :proteinas
    
-    def initialize(nombre)
-      @nombre = nombre
+    def initialize(nombre, proteinas)
+      @nombre, @proteinas = nombre, proteinas
     end
     
     def to_s
         return @nombre
+    end
+    
+    def get_proteinas
+        return @proteinas
     end
     
 end
--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -2,17 +2,21 @@ require "spec_helper"
 require "./lib/alimento/alimento.rb"
 
 RSpec.describe Alimento do
-  @nombre = "Compota de manzana"
+  @nombre = "Huevo frito"
   
   it "tiene un numero de version" do
     expect(Alimento::VERSION).not_to be nil
     
   end
-  
-  compota = Food.new(@nombre)
+  huevo = Food.new(@nombre, 14.1)
   
   it "tiene un nombre" do
-    expect(compota.to_s).not_to be nil
+    expect(huevo.to_s).not_to be nil
+    expect(huevo.to_s).to eq("Huevo frito")
+  end
+  
+  it "tiene unas proteinas" do
+    expect(huevo.get_proteinas).not_to be nil
   end
 
 end
