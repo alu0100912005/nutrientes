9701f4485370f2ecd7a175df69128837f9bceb6b
nico_apache <nico10907@hotmail.com>
2017-11-22 09:05:44 +0000

    push de docs

--- spec/alimento_spec.rb
+++ spec/alimento_spec.rb
@@ -2,7 +2,10 @@ require "spec_helper"
 require "./lib/alimento/alimento"
 require "./lib/alimento/lista"
 
-
+# @author Nicolangelo Famiglietti
+# @note Clase heredada de Food
+# == Returns:
+# Pruebas con el AIBC de alimento imperativo
 
 RSpec.describe Aibc do
   before :all do
@@ -11,7 +14,6 @@ RSpec.describe Aibc do
     glucosa = [[4.9,5.3,5.9,6.7,7.2,7.6,8.0,8.2,8.2,8.4,8.3,8.3,8.0,7.5,7.1,6.8,6.8,6.9,6.8,6.3,6.2,6.3,6.2,6.3,6.1],
                   [6.3,5.4,5.6,5.7,6.5,7.4,7.9,7.4,7.7,7.9,7.9,7.8,7.8,7.8,8.0,8.5,9.4,10.8,10.5,9.1,8.9,8.3,7.7,7.6,7.5]]
     @yogurt = Aibc.new("Yogurt"     , 3.8 , 4.9,  3.8, valores_yo, glucosa)
-    # @yogurt1 = Aibc.new("Yogurt"     , 3.8 , 4.9,  3.8, valores[1])
   end
   
   context "#Aibc - Imperativo" do
